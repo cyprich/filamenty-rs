062d6598e62da0a396e4826dc176e7f93fd161c3
cyprich <cypooriginal@gmail.com>
2025-11-20 21:44:03 +0100

    backend/app basic actix server

--- backend/app/src/main.rs
+++ backend/app/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/api")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new()
+        .service(hello))
+        .bind(("127.0.0.1", 5000))?
+        .run()
+        .await
 }
