01a5322b1315c068bdb2dbd4b92f4529e8efdd5a
cyprich <cypooriginal@gmail.com>
2025-12-08 19:43:22 +0100

    added some endpoints all returning `NotImplemented` for now

--- backend/app/src/main.rs
+++ backend/app/src/main.rs
@@ -1,15 +1,41 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware::NormalizePath, web};
 
-#[get("/api")]
+#[get("")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[get("/filaments")]
+async fn get_filaments() -> impl Responder {
+    HttpResponse::NotImplemented()
+}
+
+#[get("/filaments/{id}")]
+async fn get_filaments_by_id(id: web::Path<usize>) -> impl Responder {
+    HttpResponse::NotImplemented()
+}
+
+#[get("/info")]
+async fn info() -> impl Responder {
+    HttpResponse::NotImplemented()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()
-        .service(hello))
-        .bind(("127.0.0.1", 5000))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(NormalizePath::new(
+                actix_web::middleware::TrailingSlash::Trim,
+            ))
+            .service(
+                web::scope("/api/v2")
+                    .service(hello)
+                    .service(get_filaments)
+                    .service(get_filaments_by_id)
+                    .service(info),
+            )
+    })
+    .bind(("127.0.0.1", 5000))?
+    .run()
+    .await
 }
